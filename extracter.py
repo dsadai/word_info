19c87dba74a83b33aaa956a9ff9a3d8984fab2fe
dsadai <40234214+dsadai@users.noreply.github.com>
2020-06-10 15:50:16 +0900

    add method for conceptnet

--- extracter.py
+++ extracter.py
@@ -1,14 +1,36 @@
 from nltk.corpus import wordnet
 import argparse
 import json
+import requests
 
-parser = argparse.ArgumentParser(description='get information from dbpedia and wordnet. Usage: python extracter.py [word]')
-parser.add_argument('--word', default='bottle', help='input word ')
+parser = argparse.ArgumentParser(description='get information from dbpedia, wordnet and conceptnet. Usage: python extracter.py [word]')
+parser.add_argument('--word', default='cattle', help='input word ')
 args = parser.parse_args()
 word = str.lower(args.word)
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+def get_info_from_conceptnet(word):
+    """
+
+    :param string word: Target word
+    :return: Return 2 hash (synonyms and type)
+    """
+    #IsA relation
+    obj = requests.get('http://api.conceptnet.io/c/en/' + word + '?rel=/r/IsA&limit=100').json()
+
+    words = {}
+    for o in obj['edges']:
+        if o['start']['language'] == 'en':
+            words[o['start']['term'].split('/')[-1]] = o['weight']
+    #type relation
+    obj = requests.get('http://api.conceptnet.io/query?start=/c/en/' + word + '&rel=/r/IsA&limit=100').json()
+    uwords = {}
+    for o in obj['edges']:
+        if o['end']['language'] == 'en':
+            uwords[o['end']['term'].split('/')[-1]] = o['weight']
+
+    return words,uwords
 
 def get_info_from_dbpedia(word):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -67,11 +89,14 @@ def get_info_from_wordnet(word):
 if __name__ == '__main__':
     i_dbpedia = get_info_from_dbpedia(word)
     i_wordnet = get_info_from_wordnet(word)
+    i_conceptnet = get_info_from_conceptnet(word)
     js = {}
     js['abst'] = i_dbpedia[1]
     js['cat'] = i_dbpedia[0]
     js['syn'] = i_wordnet[0]
     js['hyp'] = i_wordnet[1]
+    js['c_syn'] = i_conceptnet[0]
+    js['c_hyp'] = i_conceptnet[0]
     print(js)
     fw = open('test.json', 'w')
     json.dump(js, fw, indent=3)
